0e5d21f408d78b5500d3fa0afaf44bb648c1d8e3
Hawar-Dzaee <hawardizayee@gmail.com>
2024-06-02 07:27:53 -0400

    coloring parameters

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ X2 = X2_mesh.flatten()
 y = (secret_w1*X1) + (secret_w2*X2)
 
 
-
-
 #-----------------------------------------
 # Plot Data & model
 
@@ -58,14 +56,42 @@ def generate_plot(w1,w2):
 
   layout = go.Layout(
     scene= dict(
-      xaxis = dict(title='X1',range=[-10,10],zeroline=True),
-      yaxis = dict(title='X2',range=[-10,10],zeroline=True),
-      zaxis = dict(title='y', range=[-10,10],zeroline=True),
+       
+      xaxis = dict( 
+         title = dict(text = 'X1',font=dict(size=18)),
+         range=[-10,10],
+         zeroline=True
+         ),
+
+      yaxis = dict(
+         title= dict(text ='X2',font=dict(size=18)),
+         range=[-10,10],
+         zeroline=True
+         ),
+
+      zaxis = dict(
+         title = dict(text = 'y',font=dict(size=18)),
+         range=[-10,10],
+         tickangle=0,
+         zeroline=True
+         ),
+
       aspectmode='cube',
+
+      # camera = dict(eye=dict(x= -3.15,y= -1.45,z=0.2))
+      camera = dict(
+                    up=dict(x=0, y=0, z=1),       # default values
+                    center=dict(x=0, y=0, z=0),   # default values 
+                    eye=dict(x=1.2, y=0.8, z=0.2)
+      ),
+
     )
+
   )
 
   figure = go.Figure(data=[Datapoints,plane],layout=layout)
+
+
   
   return figure
 
@@ -107,7 +133,7 @@ def loss_landscape(w1,w2):
     y = W2_mesh,
     z = losses_T.reshape(W1_mesh.shape),
     name = "loss functions landscape",
-    opacity = 0.4,
+    opacity = 0.5,
   )
 
   Global_minima = go.Scatter3d(
@@ -159,11 +185,10 @@ st.title("Linear Regression : Two Features, No Bias")
 st.write('By : Hawar Dzaee')
 
 
-
 with st.sidebar:
     st.subheader("Adjust the parameters to minimize the loss")
-    w1_val = st.slider("weight 1:  ($w_{1}$)", min_value=-3.0, max_value=3.0, step=0.1, value=-2.5)
-    w2_val = st.slider("weight 2:  ($w_{2}$)", min_value=-3.0, max_value=3.0, step=0.1, value=-2.2)
+    w1_val = st.slider("weight 1:  ($w_{1}$)", min_value=-3.0, max_value=3.0, step=0.1, value= -0.2)
+    w2_val = st.slider("weight 2:  ($w_{2}$)", min_value=-3.0, max_value=3.0, step=0.1, value= -2.2)
 
 
 
@@ -183,7 +208,8 @@ with container:
     with col1:
         figure_1 = generate_plot(w1_val, w2_val)
         st.plotly_chart(figure_1, use_container_width=True, aspect_ratio=5.0)  # Change aspect ratio to 1.0
-        st.latex(r'''\hat{y} = w_{1}X_{1} + w_{2}X_{2}''')
+        st.latex(r'''\hat{y} = \color{green}{w_{1}}\color{black}X_{1} \color{black}+ \color{green}{w_{2}}\color{black}X_{2}''')
+        st.latex(fr'''\hat{{y}} = {w1_val}X_{{1}} + {w2_val}X_{{2}}''')
 
   
 
@@ -192,4 +218,7 @@ with container:
         figure_2 = loss_landscape(w1_val, w2_val)
         st.plotly_chart(figure_2, use_container_width=True, aspect_ratio=5.0)
         st.latex(r"""\text{MSE}(w_{1},w_{2}) = \frac{1}{n} \sum_{i=1}^n (y_{i} - (w_{1} X_{1} + w_{2} X_{2}))^2""")
+        st.latex(fr"""\text{{MSE}}({w1_val:.2f},{w2_val:.2f}) = \frac{{1}}{{n}} \sum_{{i=1}}^n (y_{{i}} - ({w1_val:.2f} X_{{1}} + {w2_val:.2f} X_{{2}}))^2""")
+
+
 
